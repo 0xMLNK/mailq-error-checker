e2ca9abdf543d4ea999a8b2ae385880d607831d6
M4sc0t <53442741+M4sc0t@users.noreply.github.com>
2020-11-25 13:23:50 +0100

    Update mailq-error-checker.py
    checkLastLog() added to prevent mail spam with the same error

--- mailq-error-checker.py
+++ mailq-error-checker.py
@@ -1,54 +1,67 @@
 """Mailq Error Checker
-Author: @derMelnik
+Author: m4sc0t
+Last Update: 2020-10-20
 
 Script to parse mailq output.
 When output contains some trigger from error_triggers file, 
 email with output line will be sent to you.
 """
-
+import sys
 import subprocess
-import logging
 import smtplib
 import os
+import logging
 
-#for crontab use full path to file
-logging.basicConfig(filename='debug-mailq-error-checker.log', 
-                    format='%(asctime)s:%(levelname)s:%(message)s', 
+logging.basicConfig(filename='/root/skripten/mailqchecker/debug-mailqchecker.log',
+                    format='%(asctime)s:%(levelname)s:%(message)s',
                     level=logging.DEBUG)
 
-def mailqcheck(cmd):
-    """Function to parse mailq output"""
+def checkLastLog():
 
+    output = mailqcheck('mailq')
+    with open("/root/skripten/mailqchecker/last-check-result.txt", "r") as myfile:
+        data = myfile.read()
+    if str(data) == str(output):
+        logging.debug('No new errors have been found since the last check.\n')
+    else:
+        logging.debug("New errors have been found since the last check. File last-check-result.txt will be overwritten.\n")
+        f = open("/root/skripten/mailqchecker/last-check-result.txt", "w")
+        f.write(str(output))
+        f.close()
+        notify_mail(output)
+
+def mailqcheck(cmd):
+
+    """Function to parse mailq output"""
     temp = subprocess.Popen([cmd], stdout=subprocess.PIPE)
     output = str(temp.communicate())
     output = output.split("\n")
     # TODO: find better way to do this
     # cut all new line symbols from output
     output = output[0].split('\\n\\n')
-    #array to save result
+    # array to save result
     res = []
-    #tempory result (juggle with data)
+    # temp result (juggle with data)
     temp_res = []
-    #array to store error triggers from file        
+    # array to store error triggers from file
     error_triggers = []
-    
-    #for crontab use full path to file
-    with open('error_triggers') as error_triggers_file:
+
+    with open('/root/skripten/mailqchecker/error_triggers') as error_triggers_file:
         for trigger_line in error_triggers_file:
-            #cut new line symbol in array element
+            # cut new line symbol in array element
             trigger_line = trigger_line.rstrip()
             error_triggers.append(trigger_line)
 
-    #write result in temp array res
+    # write result in temp array res
     for output_line in output:
-          temp_res.append(output_line)
-          
-    #check if some element from error_triggers occurs in an array temp_res
+        temp_res.append(output_line)
+
+    # check if some element from error_triggers occurs in an array temp_res
     for trigger in error_triggers:
         trigger_value = trigger
         for element in temp_res:
             if trigger_value in element:
-                #cut new line and paste occurs element in final array res
+                # cut new line and paste occurs element in final array res
                 element = ' '.join(element.split()).replace("\\n", "")
                 res.append(element)
                 logging.debug(res)
@@ -81,10 +94,4 @@ def notify_mail(text):
 
 
 if __name__ == '__main__':
-    
-    #if array is not empty send mail otherwise write into log
-    res_arr = mailqcheck('mailq')
-    if not res_arr:
-        logging.debug('No problems with defined triggers found.')
-    else:
-        notify_mail(res_arr)
+    checkLastLog()
